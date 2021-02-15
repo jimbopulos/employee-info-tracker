32630a91cc5fed4ae5bcb3597b131fe8387ae045
James Galantino <james_galantino@aol.com>
2021-02-15 18:03:24 -0500

    Added viewDepartments function, inserted placeholder data item for test

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+// Dependencies
 const fs = require("fs");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // port
+    port: 3306,
+  
+    // username
+    user: 'root',
+  
+    // password and database path
+    password: 'sUsh!8585',
+    database: 'employeeDB',
+});
+
+// query functions
+    // View Departments
+    function viewDepartments() {
+        connection.query('SELECT * FROM department', (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            connection.end();
+        });
+    };
+viewDepartments();
+
+// prompt for user (inquirer)
+    // what would you like to do?
+    // add
+        // department
+        // employee
+        // role
+    // view
+        // departments 
+        // roles
+        // employees
+    // update employee roles
